11a87a678fd4203a9e8fc3d32c5aba4c9626f918
MichaelDChristopher <Michaeldchristopher@gmail.com>
2017-10-29 18:24:05 -0500

    some test script

---
+++ test.sh
@@ -0,0 +1,19 @@
+python3 ~/Documents/GitHub/ImageGenerator/main.py &
+sleep 1
+echo "si girl.jpg"|nc localhost 2000
+sleep 2
+echo "si girl2.jpg"|nc localhost 2000
+sleep 2
+echo "si girl3.jpg"|nc localhost 2000
+sleep 2
+echo "si girl4.jpg"|nc localhost 2000
+sleep 2
+echo "si girl5.jpg"|nc localhost 2000
+sleep 2
+echo "si girl6.jpg"|nc localhost 2000
+sleep 2
+echo "quit" | nc localhost 2000
+echo "test completed"
+
+
+
